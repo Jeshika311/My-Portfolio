cc72af43c654d9ffe65ff0184c31d02dfbc3de82
jeshika311 <jeshikatha@gmail.com>
2026-01-18 07:58:34 +0530

    add certifications

--- portfolio/frontend/src/components/CertificationsData.jsx
+++ portfolio/frontend/src/components/CertificationsData.jsx
@@ -5,6 +5,15 @@ import hackskill from "../assets/Hack2skill.png";
 import AgenticAI from "../assets/microsoft.png";
 import web from "../assets/skillcraft.png";
 import kg from "../assets/kgcoding.png";
+import uiux from "../assets/ui-ux.png";
+import msit from "../assets/msit-devtown.png";
+import tcs from "../assets/tcs.png";
+import gdg_vit from "../assets/gdg-vit.png";
+import ecell from "../assets/ecell.png";
+import codsoft from "../assets/codsoft.png";
+import codeclash from "../assets/codeclash.png";
+import hackvision from "../assets/hackvision.png";
+import hackureka from "../assets/hackureka.png";
 
 export const certifications = [
   {
@@ -45,6 +54,7 @@ export const certifications = [
     year: "2025",
     month: "July",
     img: AgenticAI,
+    link: "https://res.cloudinary.com/dbavu8clp/image/upload/v1768657917/Screenshot_2026-01-17_192057_utpha5.png"
   },
   {
     name: "Web development Intern",
@@ -52,13 +62,86 @@ export const certifications = [
     year: "2025",
     month: "June",
     img: web,
+    link: "https://res.cloudinary.com/dbavu8clp/image/upload/v1768657919/Screenshot_2026-01-17_192138_tpmtsj.png"
   },
   {
     name: "Backend Developer",
     org: "KG Coding",
     year: "2025",
-    month: "october",
+    month: "October",
     img: kg,
     link: "https://www.knowledgegate.ai/student/certificates/CERT-1759666251061-6BCFB9A6"
+  },
+  {
+    name: "Pitching competition",
+    org: "e-cell MMDU",
+    year: "2024",
+    month: "November",
+    img: ecell,
+    link: "https://res.cloudinary.com/dbavu8clp/image/upload/v1768660208/e-cell_business_pitching_11-11-2024_oyxd9q.jpg"
+  },
+  {
+    name: "Soft skills",
+    org: "tcs iON",
+    year: "2025",
+    month: "April",
+    img: tcs,
+    link: "https://res.cloudinary.com/dbavu8clp/image/upload/v1768657586/Screenshot_2026-01-17_183557_bivoz8.png"
+  },
+  {
+    name: "UI/UX Design",
+    org: "Alpha intern",
+    year: "2024",
+    month: "September",
+    img: uiux,
+    link: "https://res.cloudinary.com/dbavu8clp/image/upload/v1768657570/Screenshot_2026-01-17_183330_qwpc0k.png"
+  },
+  {
+    name: "Python Internship",
+    org: "Codesoft",
+    year: "2025",
+    month: "July",
+    img: codsoft,
+    link: "https://res.cloudinary.com/dbavu8clp/image/upload/v1768657588/Screenshot_2026-01-17_183717_eer6pk.png"
+  },
+  {
+    name: "Web Security bootcamp",
+    org: "GDG VIT",
+    year: "2025",
+    month: "June",
+    img: gdg_vit,
+    link: "https://res.cloudinary.com/dbavu8clp/image/upload/v1768657559/Screenshot_2026-01-17_183241_n50skc.png"
+  },
+  {
+    name: "Web Security bootcamp",
+    org: "Microsoft Student Chapter - MSIT",
+    year: "2025",
+    month: "June",
+    img: msit,
+    link: "https://res.cloudinary.com/dbavu8clp/image/upload/v1768657438/Screenshot_2026-01-17_183223_push4f.png"
+  },
+  {
+    name: "Code clash hackathon",
+    org: "Microsoft Student Chapter - MSIT",
+    year: "2025",
+    month: "June",
+    img: codeclash,
+    link: "https://res.cloudinary.com/dbavu8clp/image/upload/v1768660209/Code_Clash_28_06_2025_vpocje.png"
+  },
+  {
+    name: "HackVision Vault 2k25",
+    org: "CSE Dept. MMDU",
+    year: "2025",
+    month: "March",
+    img: hackvision,
+    link: "https://res.cloudinary.com/dbavu8clp/image/upload/v1768660208/HackVisionVault_26-03-2025_b7gn6m.jpg"
+  },
+  {
+    name: "HACKURE-ka",
+    org: "GDG MMDU",
+    year: "2025",
+    month: "February",
+    img: hackureka,
+    link: "https://res.cloudinary.com/dbavu8clp/image/upload/v1768660209/GDG_Hackure-ka_15-02-2025_yhzgtt.png"
   }
 ];
